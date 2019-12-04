52b5f2767355300d67a6627fb8d2b051b2366527
BFisch14 <bradfisch142010@gmail.com>
2019-12-04 12:33:37 -0800

    fix Blank case

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -146,7 +146,15 @@ TEST(getWinner, invalidWinner)
 TEST(getWinner, blankWinner)
 {
 	TicTacToeBoard ticTac;
+	ticTac.placePiece(0, 0);
+	ticTac.placePiece(0, 1);
 	ticTac.placePiece(0, 2);
+	ticTac.placePiece(1, 0);
+	ticTac.placePiece(1, 1);
+	ticTac.placePiece(1, 2);
+	ticTac.placePiece(2, 0);
+	ticTac.placePiece(2, 1);
+	ticTac.placePiece(2, 2);
 	ASSERT_EQ(ticTac.getWinner(), Blank);
 }
 
